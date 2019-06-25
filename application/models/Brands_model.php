ccea82885b854dbaabb6f612fd372ebc51c12e3f
linaseniorwebdev <lina.web.developer.senior@gmail.com>
2019-06-25 06:12:36 -0300

    Some Update

--- application/models/Brands_model.php
+++ application/models/Brands_model.php
@@ -16,7 +16,7 @@ class Brands_model extends CI_Model {
 	 * @param null $status
 	 * @return mixed
 	 */
-	public function get_all_brands($status = null, $type = null) {
+	public function get_all_brands($type = null, $status = null) {
 		$this->db->order_by('sales', 'desc');
 
 		if ($type !== null) {
